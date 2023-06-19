5c9b901c09caaf5e51e6fcf026f4f3931d213078
Chris Hayen <chayen@gmail.com>
2023-06-19 17:29:23 -0400

    Added rates service

---
+++ rates/Program.cs
@@ -0,0 +1,37 @@
+﻿using NATS.Client;
+using System.Text;
+using System.Text.Json;
+
+ConnectionFactory cf = new ConnectionFactory();
+IConnection c = cf.CreateConnection();
+
+string json;
+using (StreamReader r = new StreamReader("rates.json"))
+{
+  json = r.ReadToEnd();
+  // List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+}
+
+
+// string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+using (ISyncSubscription s = c.SubscribeSync("rates"))
+{
+  while (true)
+  {
+    Msg msg = s.NextMessage();
+    string msgSubject = msg.Subject;
+    string msgPayload = Encoding.ASCII.GetString(msg.Data);
+    c.Publish(new Msg(msg.Reply, Encoding.ASCII.GetBytes(json)));
+    c.Flush();
+  }
+}
+
+
+
+
+
+
+
+
+
