66dd15279cfcc2ba6190d49643785025511c2e53
Chris Hayen <chayen@gmail.com>
2023-06-19 01:47:17 -0400

    /student-loans

--- apply/app/student-loans/page.tsx
+++ apply/app/student-loans/page.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>sdf</h1>;
+  return (
+    <div>
+      <div style={{ backgroundColor: "#23233f" }} className="py-9 px-24">
+        <Link href="/">
+          <Image
+            src="/discover-logo@2x.png"
+            width="180"
+            height="30"
+            alt="discover logo"
+          />
+        </Link>
+      </div>
+      <div className="h-14 border-b border-b-slate-200">&nbsp;</div>
+      <div style={{ backgroundColor: "#23233f" }} className="h-96">
+        &nbsp;
+      </div>
+      <div className="grid grid-cols-3 px-24 py-10 tracking-wide">
+        <div>
+          <p className="text-2xl font-medium" style={{ color: "#ec6b29" }}>
+            New Lower Fixed Rates
+          </p>
+        </div>
+        <div>
+          <p className="text-2xl font-medium">Variable Rates</p>
+        </div>
+      </div>
+    </div>
+  );
 }
